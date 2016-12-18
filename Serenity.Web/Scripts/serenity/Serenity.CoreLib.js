835e6613f7e97cb536c364d27f5d689c1a32b4ce
Theodoros Christofis <theoc1970@hotmail.com>
2016-12-18 12:53:26 +0200

    set Typescript version to 2.1

--- Serenity.Web/Scripts/serenity/Serenity.CoreLib.js
+++ Serenity.Web/Scripts/serenity/Serenity.CoreLib.js
@@ -825,33 +825,33 @@ var Q;
             }
             return t;
         };
-        LT.$table = {};
-        LT.empty = new LT('');
-        LT.initializeTextClass = function (type, prefix) {
-            var $t1 = Object.keys(type).slice();
-            for (var $t2 = 0; $t2 < $t1.length; $t2++) {
-                var member = $t1[$t2];
-                var value = type[member];
-                if (value instanceof LT) {
-                    var lt = value;
-                    var key = prefix + member;
-                    LT.$table[key] = lt.key;
-                    type[member] = new LT(key);
-                }
-            }
-        };
-        LT.getDefault = function (key, defaultText) {
-            var t = LT.$table[key];
-            if (t == null) {
-                t = defaultText;
-                if (t == null) {
-                    t = key || '';
-                }
-            }
-            return t;
-        };
         return LT;
     }());
+    LT.$table = {};
+    LT.empty = new LT('');
+    LT.initializeTextClass = function (type, prefix) {
+        var $t1 = Object.keys(type).slice();
+        for (var $t2 = 0; $t2 < $t1.length; $t2++) {
+            var member = $t1[$t2];
+            var value = type[member];
+            if (value instanceof LT) {
+                var lt = value;
+                var key = prefix + member;
+                LT.$table[key] = lt.key;
+                type[member] = new LT(key);
+            }
+        }
+    };
+    LT.getDefault = function (key, defaultText) {
+        var t = LT.$table[key];
+        if (t == null) {
+            t = defaultText;
+            if (t == null) {
+                t = key || '';
+            }
+        }
+        return t;
+    };
     Q.LT = LT;
 })(Q || (Q = {}));
 var Q;
@@ -2340,7 +2340,6 @@ var Serenity;
         return [field];
     }
     Serenity.Criteria = Criteria;
-    var Criteria;
     (function (Criteria) {
         var C = Criteria;
         function isEmpty(c) {
@@ -2553,13 +2552,13 @@ var Serenity;
 /// <reference path="../../../../typings/jquery.validation/jquery.validation.d.ts" />
 var Serenity;
 (function (Serenity) {
-    var Widget = (function () {
+    var Widget = Widget_1 = (function () {
         function Widget(element, options) {
             var _this = this;
             this.element = element;
             this.options = options || {};
-            this.widgetName = Widget.getWidgetName(ss.getInstanceType(this));
-            this.uniqueName = this.widgetName + (Widget.nextWidgetNumber++).toString();
+            this.widgetName = Widget_1.getWidgetName(ss.getInstanceType(this));
+            this.uniqueName = this.widgetName + (Widget_1.nextWidgetNumber++).toString();
             if (element.data(this.widgetName)) {
                 throw new ss.Exception(Q.format("The element already has widget '{0}'!", this.widgetName));
             }
@@ -2628,7 +2627,7 @@ var Serenity;
                 params.element && params.element(widget.element);
             }
             else {
-                var e = Widget.elementFor(params.type);
+                var e = Widget_1.elementFor(params.type);
                 if (params.container)
                     e.appendTo(params.container);
                 params.element && params.element(e);
@@ -2654,24 +2653,26 @@ var Serenity;
             }
             return this.asyncPromise;
         };
-        Widget.nextWidgetNumber = 0;
-        Widget = __decorate([
-            Serenity.Decorators.registerClass()
-        ], Widget);
         return Widget;
     }());
+    Widget.nextWidgetNumber = 0;
+    Widget = Widget_1 = __decorate([
+        Serenity.Decorators.registerClass()
+    ], Widget);
     Serenity.Widget = Widget;
+    var Widget_1;
 })(Serenity || (Serenity = {}));
 var Serenity;
 (function (Serenity) {
-    var TemplatedWidget = (function (_super) {
+    var TemplatedWidget = TemplatedWidget_1 = (function (_super) {
         __extends(TemplatedWidget, _super);
         function TemplatedWidget(container, options) {
-            _super.call(this, container, options);
-            this.idPrefix = this.uniqueName + '_';
-            var widgetMarkup = this.getTemplate().replace(new RegExp('~_', 'g'), this.idPrefix);
+            var _this = _super.call(this, container, options) || this;
+            _this.idPrefix = _this.uniqueName + '_';
+            var widgetMarkup = _this.getTemplate().replace(new RegExp('~_', 'g'), _this.idPrefix);
             widgetMarkup = Q.jsRender(widgetMarkup);
-            this.element.html(widgetMarkup);
+            _this.element.html(widgetMarkup);
+            return _this;
         }
         TemplatedWidget.prototype.byId = function (id) {
             return $('#' + this.idPrefix + id);
@@ -2689,8 +2690,8 @@ var Serenity;
             };
             var type = ss.getInstanceType(this);
             var fullName = ss.getTypeFullName(type);
-            var templateNames = TemplatedWidget.templateNames;
-            var cachedName = TemplatedWidget.templateNames[fullName];
+            var templateNames = TemplatedWidget_1.templateNames;
+            var cachedName = TemplatedWidget_1.templateNames[fullName];
             if (cachedName != null) {
                 return cachedName;
             }
@@ -2716,7 +2717,7 @@ var Serenity;
                 name = noGeneric(ss.getTypeName(type));
                 if (Q.canLoadScriptData('Template.' + name) ||
                     $('script#Template_' + name).length > 0) {
-                    TemplatedWidget.templateNames[fullName] = name;
+                    TemplatedWidget_1.templateNames[fullName] = name;
                     return name;
                 }
                 type = ss.getBaseType(type);
@@ -2736,13 +2737,14 @@ var Serenity;
             }
             return template;
         };
-        TemplatedWidget.templateNames = {};
-        TemplatedWidget = __decorate([
-            Serenity.Decorators.registerClass()
-        ], TemplatedWidget);
         return TemplatedWidget;
     }(Serenity.Widget));
+    TemplatedWidget.templateNames = {};
+    TemplatedWidget = TemplatedWidget_1 = __decorate([
+        Serenity.Decorators.registerClass()
+    ], TemplatedWidget);
     Serenity.TemplatedWidget = TemplatedWidget;
+    var TemplatedWidget_1;
 })(Serenity || (Serenity = {}));
 var Serenity;
 (function (Serenity) {
@@ -2770,17 +2772,18 @@ var Serenity;
 })(Serenity || (Serenity = {}));
 var Serenity;
 (function (Serenity) {
-    var TemplatedDialog = (function (_super) {
+    var TemplatedDialog = TemplatedDialog_1 = (function (_super) {
         __extends(TemplatedDialog, _super);
         function TemplatedDialog(options) {
-            _super.call(this, Q.newBodyDiv(), options);
-            this.isPanel = ss.getAttributes(ss.getInstanceType(this), Serenity.PanelAttribute, true).length > 0;
-            if (!this.isPanel) {
-                this.initDialog();
+            var _this = _super.call(this, Q.newBodyDiv(), options) || this;
+            _this.isPanel = ss.getAttributes(ss.getInstanceType(_this), Serenity.PanelAttribute, true).length > 0;
+            if (!_this.isPanel) {
+                _this.initDialog();
             }
-            this.initValidator();
-            this.initTabs();
-            this.initToolbar();
+            _this.initValidator();
+            _this.initTabs();
+            _this.initToolbar();
+            return _this;
         }
         TemplatedDialog.getCssSize = function (element, name) {
             var cssSize = element.css(name);
@@ -2801,16 +2804,16 @@ var Serenity;
             var dialog = $('<div/>').hide().addClass(dialogClass).appendTo(document.body);
             try {
                 var sizeHelper = $('<div/>').addClass('size').appendTo(dialog);
-                size = TemplatedDialog.getCssSize(sizeHelper, 'minWidth');
+                size = TemplatedDialog_1.getCssSize(sizeHelper, 'minWidth');
                 if (size != null)
                     opt.minWidth = size;
-                size = TemplatedDialog.getCssSize(sizeHelper, 'width');
+                size = TemplatedDialog_1.getCssSize(sizeHelper, 'width');
                 if (size != null)
                     opt.width = size;
-                size = TemplatedDialog.getCssSize(sizeHelper, 'height');
+                size = TemplatedDialog_1.getCssSize(sizeHelper, 'height');
                 if (size != null)
                     opt.height = size;
-                size = TemplatedDialog.getCssSize(sizeHelper, 'minHeight');
+                size = TemplatedDialog_1.getCssSize(sizeHelper, 'minHeight');
                 if (size != null)
                     opt.minHeight = size;
             }
@@ -2941,7 +2944,7 @@ var Serenity;
             var dialogClass = 's-Dialog ' + this.getCssClass();
             opt.dialogClass = dialogClass;
             opt.width = 920;
-            TemplatedDialog.applyCssSizes(opt, dialogClass);
+            TemplatedDialog_1.applyCssSizes(opt, dialogClass);
             opt.autoOpen = false;
             var type = ss.getInstanceType(this);
             opt.resizable = ss.getAttributes(type, Serenity.ResizableAttribute, true).length > 0;
@@ -3019,12 +3022,13 @@ var Serenity;
                 }
             }
         };
-        TemplatedDialog = __decorate([
-            Serenity.Decorators.registerClass([Serenity.IDialog])
-        ], TemplatedDialog);
         return TemplatedDialog;
     }(Serenity.TemplatedWidget));
+    TemplatedDialog = TemplatedDialog_1 = __decorate([
+        Serenity.Decorators.registerClass([Serenity.IDialog])
+    ], TemplatedDialog);
     Serenity.TemplatedDialog = TemplatedDialog;
+    var TemplatedDialog_1;
 })(Serenity || (Serenity = {}));
 /// <reference path="../../../../typings/sortablejs/sortablejs.d.ts" />
 var Serenity;
@@ -3032,9 +3036,8 @@ var Serenity;
     var ColumnPickerDialog = (function (_super) {
         __extends(ColumnPickerDialog, _super);
         function ColumnPickerDialog() {
-            var _this = this;
-            _super.call(this);
-            new Serenity.QuickSearchInput(this.byId("Search"), {
+            var _this = _super.call(this) || this;
+            new Serenity.QuickSearchInput(_this.byId("Search"), {
                 onSearch: function (fld, txt, done) {
                     txt = Q.trimToNull(txt);
                     if (txt != null)
@@ -3044,8 +3047,9 @@ var Serenity;
                     });
                 }
             });
-            this.ulVisible = this.byId("VisibleCols");
-            this.ulHidden = this.byId("HiddenCols");
+            _this.ulVisible = _this.byId("VisibleCols");
+            _this.ulHidden = _this.byId("HiddenCols");
+            return _this;
         }
         ColumnPickerDialog.createToolButton = function (grid) {
             return {
@@ -3223,13 +3227,13 @@ var Serenity;
         ColumnPickerDialog.prototype.getTemplate = function () {
             return "\n<div class=\"search\"><input id=\"~_Search\" type=\"text\" disabled /></div>\n<div class=\"columns-container\">\n<div class=\"column-list visible-list bg-success\">\n  <h5><i class=\"icon-eye\"></i> " + Q.text("Controls.ColumnPickerDialog.VisibleColumns") + "</h5>\n  <ul id=\"~_VisibleCols\"></ul>\n</div>\n<div class=\"column-list hidden-list bg-info\">\n  <h5><i class=\"icon-list\"></i> " + Q.text("Controls.ColumnPickerDialog.HiddenColumns") + "</h5>\n  <ul id=\"~_HiddenCols\"></ul>\n</div>\n</div>";
         };
-        ColumnPickerDialog = __decorate([
-            Serenity.Decorators.registerClass(),
-            Serenity.Decorators.resizable(),
-            Serenity.Decorators.responsive()
-        ], ColumnPickerDialog);
         return ColumnPickerDialog;
     }(Serenity.TemplatedDialog));
+    ColumnPickerDialog = __decorate([
+        Serenity.Decorators.registerClass(),
+        Serenity.Decorators.resizable(),
+        Serenity.Decorators.responsive()
+    ], ColumnPickerDialog);
     Serenity.ColumnPickerDialog = ColumnPickerDialog;
 })(Serenity || (Serenity = {}));
 var Serenity;
@@ -3493,7 +3497,6 @@ var Slick;
         var GroupItemMetadataProvider;
     })(Data = Slick.Data || (Slick.Data = {}));
 })(Slick || (Slick = {}));
-var Slick;
 (function (Slick) {
     var RemoteView = (function () {
         function RemoteView(options) {
@@ -4543,7 +4546,6 @@ var Slick;
     }());
     Slick.RemoteView = RemoteView;
 })(Slick || (Slick = {}));
-var Slick;
 (function (Slick) {
     var Aggregators;
     (function (Aggregators) {
